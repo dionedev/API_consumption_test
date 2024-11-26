4451f5fea52170a1bff8b9459439e37a775a94bf
Dione Santos <dionedev@gmail.com>
2024-11-26 13:08:16 -0300

    feat: class to monitor ui states

---
+++ lib/ui/modules/home/stores/product_store.dart
@@ -0,0 +1,32 @@
+import 'package:consumoapi/data/http/exceptions/not_found_exception.dart';
+import 'package:consumoapi/data/models/product_model.dart';
+import 'package:consumoapi/data/repositories/product_repository.dart';
+import 'package:flutter/material.dart';
+
+class ProductStore {
+  final IProductRepository repository;
+
+  final ValueNotifier<bool> isLoading = ValueNotifier<bool>(false);
+
+  final ValueNotifier<List<ProductModel>> state =
+      ValueNotifier<List<ProductModel>>([]);
+
+  final ValueNotifier<String> erro = ValueNotifier<String>('');
+
+  ProductStore({required this.repository});
+
+  Future getProducts() async {
+    isLoading.value = true;
+
+    try {
+      final result = await repository.getProducts();
+      state.value = result;
+    } on NotFoundException catch (e) {
+      erro.value = e.message;
+    } catch (e) {
+      erro.value = e.toString();
+    }
+
+    isLoading.value = false;
+  }
+}
