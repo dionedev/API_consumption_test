540d69db15b7f20b33432de362e7838367696395
Dione Santos <dionedev@gmail.com>
2024-11-26 13:23:06 -0300

    feat: home page

--- lib/ui/modules/home/home_page.dart
+++ lib/ui/modules/home/home_page.dart
@@ -1,3 +1,6 @@
+import 'package:consumoapi/data/http/http_client.dart';
+import 'package:consumoapi/data/repositories/product_repository.dart';
+import 'package:consumoapi/ui/modules/home/stores/product_store.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,13 +11,129 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final ProductStore productStore = ProductStore(
+    repository: ProductRepository(
+      client: HttpClient(),
+    ),
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    productStore.getProducts();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Consumindo uma API'),
+        title: const Text(
+          'Consumindo uma API',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        backgroundColor: Colors.green,
       ),
-      body: Container(),
+      body: AnimatedBuilder(
+          animation: Listenable.merge([
+            productStore.isLoading,
+            productStore.state,
+            productStore.erro,
+          ]),
+          builder: (context, child) {
+            if (productStore.isLoading.value) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            if (productStore.erro.value.isNotEmpty) {
+              return Center(
+                child: Text(
+                  productStore.erro.value,
+                  style: const TextStyle(
+                    color: Colors.black54,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              );
+            }
+
+            if (productStore.state.value.isEmpty) {
+              return const Center(
+                child: Text(
+                  'Nenhum item na lista',
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              );
+            } else {
+              return ListView.separated(
+                separatorBuilder: (context, index) => const SizedBox(
+                  height: 32,
+                ),
+                itemCount: productStore.state.value.length,
+                itemBuilder: (_, index) {
+                  final item = productStore.state.value[index];
+                  return Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(16),
+                        child: Image.network(
+                          item.thumbnail,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      ListTile(
+                        contentPadding: EdgeInsets.zero,
+                        title: Text(
+                          item.title,
+                          style: const TextStyle(
+                            color: Colors.black54,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 24,
+                          ),
+                        ),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'R\$ ${item.price}',
+                              style: const TextStyle(
+                                color: Colors.green,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 20,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 4,
+                            ),
+                            Text(
+                              item.description,
+                              style: const TextStyle(
+                                color: Colors.black54,
+                                fontSize: 18,
+                              ),
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 2,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              );
+            }
+          }),
     );
   }
 }
