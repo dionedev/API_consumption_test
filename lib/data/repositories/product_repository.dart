409cc6a03e395f64cdd9a081903e88ff3bdb12c4
Dione Santos <dionedev@gmail.com>
2024-11-26 13:09:55 -0300

    feat: api request

--- lib/data/repositories/product_repository.dart
+++ lib/data/repositories/product_repository.dart
@@ -1,3 +1,6 @@
+import 'dart:convert' as convert;
+import 'package:consumoapi/data/http/exceptions/not_found_exception.dart';
+import 'package:consumoapi/data/http/http_client.dart';
 import 'package:consumoapi/data/models/product_model.dart';
 
 abstract class IProductRepository {
@@ -5,9 +8,29 @@ abstract class IProductRepository {
 }
 
 class ProductRepository implements IProductRepository {
+  final IHttpClient client;
+
+  ProductRepository({required this.client});
+
   @override
-  Future<List<ProductModel>> getProducts() {
-    // TODO: implement getProducts
-    throw UnimplementedError();
+  Future<List<ProductModel>> getProducts() async {
+    final response = await client.get(url: 'https://dummyjson.com/products');
+
+    if (response.statusCode == 200) {
+      final List<ProductModel> products = [];
+
+      final body = convert.jsonDecode(response.body);
+
+      body['products'].map((item) {
+        final ProductModel product = ProductModel.fromMap(item);
+        products.add(product);
+      }).toList();
+
+      return products;
+    } else if (response.statusCode == 404) {
+      throw NotFoundException('A url informada não é válida');
+    } else {
+      throw Exception('Não foi possível carregar os produtos');
+    }
   }
 }
